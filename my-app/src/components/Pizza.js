0fe101d26e44a1f8f2191871f51ac671f9b8a909
Timi <timeaher@gmail.com>
2022-11-30 16:11:15 +0100

    css correction

--- my-app/src/components/Pizza.js
+++ my-app/src/components/Pizza.js
@@ -1,20 +1,21 @@
 import React, { useState } from "react";
+import './Pizza.css'
 
 function Pizza({name, id, picture, ingredients, price}) {
    /* const pizzaName = pizzaData.name
     const{name} = pizzaData /*destructing, ua. mint a felső*. megadhatjuk egyenként és a a sorredn is mindegy objektumnál*/
   
-  const [isFavourite, setIsFavourite] = useState(false)
+  const [isFavorite, setIsFavorite] = useState(false)
 
     return (
     <div className =
-    {"Pizza" +(isFavourite? ' fav' : '')}>
+    {"Pizza" +(isFavorite ? ' fav' : '')}>
     <img onClick={() =>
-      setIsFavourite(true)
+      setIsFavorite(true)
     }
     src="https://as1.ftcdn.net/v2/jpg/01/09/84/42/1000_F_109844239_A7MdQSDf4y1H80cfvHZuSa0zKBkZ68S7.jpg" alt ="csillag" 
-    className="favourite"/>
-    <h1>{name} {isFavourite? 'Kedvenc': 'Fújjj'}</h1>
+    className="favorite"/>
+    <h1>{name} {isFavorite? 'Kedvenc': 'Fújjj'}</h1>
     <p>{id}</p>
     <h3>{ingredients}</h3>
     <h2>{price}EUR</h2>
